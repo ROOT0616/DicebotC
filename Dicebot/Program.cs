d3eff04b173d1d260c9d7473b63a7e247ac06624
RooT0616 <matumokencave@gmail.com>
2021-03-03 13:23:36 +0900

    2021/03/03   ダイスを複数個を複数回振ることができるように修正 ついでに、判定も追加

--- Dicebot/Program.cs
+++ Dicebot/Program.cs
@@ -61,6 +61,7 @@ namespace Dicebot
 
       //ここから記述--------------------------------------------------------------------------
       var CommandContext = message.Content;
+      string Dicecount;//ダイスの面の数
       string DiceNum;//ダイスの面の数
       string NumTimes;//ダイスを振る回数
       string ClearNum;//危険度
@@ -69,6 +70,7 @@ namespace Dicebot
 
       int Random_basic(string DiceNum)
       {
+        seed++;
         Random rnd = new Random(seed++);
         int m1 = rnd.Next(1, int.Parse(DiceNum) + 1);
         return m1;
@@ -104,13 +106,15 @@ namespace Dicebot
             Random rnd = new Random(seed++);
             string m0 = "";
             string m1 = rnd.Next(1, DiceNum1 + 1).ToString();
-            m0 += $"<@{id}>" + "　:　" + DiceNum1 + "面ダイス" + "　:　" + m1 + "\n";
+            m0 += $"<@{id}>" + "　:　" + DiceNum1 + "面ダイス" + "　:　出目は " + m1 + " です。\n";
             await message.Channel.SendMessageAsync(m0);
           }
+          //ダイスを振る回数が10回を超える場合
           else if (int.TryParse(CommandContext.Substring(CommandContext.IndexOf("n") + 1), out int NumTimes1) && NumTimes1 > 10)
           {
             await message.Channel.SendMessageAsync($"<@{id}>" + "　:　" + "10回以下でお願いします");
           }
+          //ダイスを10回以下振る場合
           else if (int.TryParse(CommandContext.Substring(CommandContext.IndexOf("n") + 1), out NumTimes1))
           {
             //ダイスの面の数
@@ -120,9 +124,10 @@ namespace Dicebot
             m0 += $"<@{id}>" + "　:　" + DiceNum + "面ダイスを" + NumTimes1 + "回ふります。" + "\n";
             for (int i = 0; i < NumTimes1; i++)
             {
+              seed += i;
               Random rnd = new Random(seed++);
               string m1 = rnd.Next(1, int.Parse(DiceNum) + 1).ToString();
-              m0 += $"<@{id}>" + "　:　" + (i + 1).ToString() + "　:　"  + m1 + "\n";
+              m0 += $"<@{id}>" + "　:　" + (i + 1).ToString() + "　:　出目は " + m1 + " です。\n";
             }
             await message.Channel.SendMessageAsync(m0);
           }
@@ -154,6 +159,7 @@ namespace Dicebot
             seed = Environment.TickCount;
             for (int i = 0; i < int.Parse(NumTimes); i++)
             {
+              seed += i + j;
               Random rnd = new Random(seed++);
               string m1 = rnd.Next(1, int.Parse(DiceNum) + 1).ToString();
               if (int.Parse(m1) >= int.Parse(ClearNum))
@@ -181,18 +187,113 @@ namespace Dicebot
       ////////////////////////////////////////////////////////////////////////////
       if (CommandContext.Substring(0, 2) == "!r")
       {
-        if (!(CommandContext.Substring(2) == ""))
+
+        if (CommandContext.Substring(2) == "")
         {
-          DiceNum = CommandContext.Substring(CommandContext.IndexOf("d") + 1);
-          NumTimes = CommandContext.Substring(2, CommandContext.IndexOf("d") - 2);
-          await message.Channel.SendMessageAsync($"<@{id}>" + "　:　" + DiceNum + "面ダイスを" + NumTimes + "個振ります");
-          List<int> ms = new List<int>();
-          for (int i = 0; i < int.Parse(NumTimes); i++)
+          string help = $"<@{id}>" + "　:「!r」の使い方説明\n";
+          help += "!r3 d4 の場合、rの後の3はダイスの個数、dの後の4はダイス面、つまり、4面ダイスを3個を1回ふることになります。\n" +
+            "!r3 d3 n4 の場合、nの後の4は振る回数になり、3面ダイスを3個を4回ふることになります。\n" +
+            "!r3 d3 n4 c1 t1 の場合、cの後の数字は成功値、tの後の数字は成功度となります。";
+          await message.Channel.SendMessageAsync(help);
+        }
+        else if (!(CommandContext[2..] == ""))
+        {
+          if (int.TryParse(CommandContext[(CommandContext.IndexOf("d") + 1)..], out int DiceNum1))
           {
-            ms.Add(Random_basic(DiceNum));
+            DiceNum = CommandContext[(CommandContext.IndexOf("d") + 1)..];
+            NumTimes = CommandContext[2..CommandContext.IndexOf("d")];
+            await message.Channel.SendMessageAsync($"<@{id}>" + "　:　" + DiceNum + "面ダイスを" + NumTimes + "個振ります");
+            List<int> ms = new List<int>();
+            for (int i = 0; i < int.Parse(NumTimes); i++)
+            {
+              ms.Add(Random_basic(DiceNum));
+            }
+            var s = String.Join(",", ms);
+            await message.Channel.SendMessageAsync($"<@{id}>" + "　:　出目は" + s + "で、それらの和は" + List_Sum(ms) + "です");
           }
-          var s = String.Join(",", ms);
-          await message.Channel.SendMessageAsync($"<@{id}>" + "　:　出目は" + s + "で、それらの和は" + List_Sum(ms) + "です");
+          else if (int.TryParse(CommandContext[(CommandContext.IndexOf("n") + 1)..], out int NumTimes1) && NumTimes1 > 10)
+          {
+            await message.Channel.SendMessageAsync($"<@{id}>" + "　:　" + "10回以下でお願いします");
+          }
+          else if (int.TryParse(CommandContext[(CommandContext.IndexOf("n") + 1)..], out NumTimes1))
+          {
+            DiceNum = CommandContext.Substring(CommandContext.IndexOf("d") + 1, CommandContext.IndexOf("n") - CommandContext.IndexOf("d") - 1);
+            NumTimes = CommandContext[2..CommandContext.IndexOf("d")];
+            List<int> ms = new List<int>();
+            string m0 = "";
+            m0 += $"<@{id}>" + "　:　" + DiceNum + "面ダイスを" + NumTimes + "個を" + NumTimes1 + "回振ります\n";
+            seed = Environment.TickCount;
+            for (int j = 0; j < NumTimes1; j++)
+            {
+              for (int i = 0; i < int.Parse(NumTimes); i++)
+              {
+                seed += i + j;
+                Random rnd = new Random(seed);
+                int m1 = rnd.Next(1, int.Parse(DiceNum) + 1);
+                ms.Add(m1);
+              }
+              var s = String.Join(" + ", ms);
+              m0 += $"<@{id}>" + "　:　" + (j + 1) + "　:　" + s + " = " + List_Sum(ms) + "\n";
+              ms.Clear();
+            }
+            await message.Channel.SendMessageAsync(m0);
+          }
+          else if (int.TryParse(CommandContext.Substring(CommandContext.IndexOf("c") + 1, CommandContext.IndexOf("t") - CommandContext.IndexOf("c") - 1), out int ClearNum1) && int.TryParse(CommandContext.Substring(CommandContext.IndexOf("t") + 1), out int ClearTimes1))
+          {
+            //  /r Dicecount  d DiceNum n NumTimes  c ClearNum  t ClearTimes
+            //ダイスを振る個数
+            Dicecount = CommandContext[2..CommandContext.IndexOf("d")];
+            //ダイスの面の数
+            DiceNum = CommandContext.Substring(CommandContext.IndexOf("d") + 1, CommandContext.IndexOf("n") - CommandContext.IndexOf("d") - 1);
+            //ダイスを振る回数
+            NumTimes = CommandContext.Substring(CommandContext.IndexOf("n") + 1, CommandContext.IndexOf("c") - CommandContext.IndexOf("n") - 1);
+            //ClearNumは成功値
+            ClearNum = CommandContext.Substring(CommandContext.IndexOf("c") + 1, CommandContext.IndexOf("t") - CommandContext.IndexOf("c") - 1);
+            //ClearTimesは成功度
+            ClearTimes = CommandContext.Substring(CommandContext.IndexOf("t") + 1);
+
+            //難易度がダイス面より大きい場合
+            if (!(int.Parse(ClearNum) < (int.Parse(DiceNum) + int.Parse(DiceNum) + 1)))
+              return;
+            //成功地がダイスを振る回数より大きいとき
+            if (!(int.Parse(ClearTimes) < int.Parse(NumTimes) + 1))
+              return;
+
+            List<int> ms = new List<int>();
+            int k = 0;
+            string m0 = "";
+            m0 += $"<@{id}>" + "　:　" + DiceNum + "面ダイス" + Dicecount + "個を" + NumTimes + "回ふります。" + "成功値" + ClearNum + "の成功度" + ClearTimes + "です。\n";
+            seed = Environment.TickCount;
+            for (int j = 0; j < int.Parse(NumTimes); j++) 
+            {
+              for (int i = 0; i < int.Parse(Dicecount); i++)
+              {
+                seed += i + j;
+                Random rnd = new Random(seed);
+                int m1 = rnd.Next(1, int.Parse(DiceNum) + 1);
+                ms.Add(m1);
+              }
+              var s = String.Join(" + ", ms);
+              if (List_Sum(ms) >= int.Parse(ClearNum))
+              {
+                k++;
+                m0 += $"<@{id}>" + "　:　" + (j + 1) + "　:　" + k + "回目の成功：" + s + " = " + List_Sum(ms) + "\n";
+                if (k == int.Parse(ClearTimes))
+                {
+                  m0 += $"<@{id}>" + "　:　" + "成功度" + k + "をクリアしたのでダイスを止めます。" + "\n";
+                  //j成功値に達するとfor文から脱出
+                  if (k == int.Parse(ClearTimes))
+                    break;
+                }
+              }
+              else
+              {
+                m0 += $"<@{id}>" + "　:　" + (j + 1) + "　:　" + "　　失敗　 ：" + s + " = " + List_Sum(ms) + "\n";
+              }
+              ms.Clear();
+            }
+            await message.Channel.SendMessageAsync(m0);
+          }
         }
       }
     }
